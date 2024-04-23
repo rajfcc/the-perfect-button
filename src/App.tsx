bf7a60d617013d66fcfb01fc6c0cd230fb8a2649
Raj Sth <sthraj74@gmail.com>
2024-04-23 20:08:13 +0545

    Project completed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import PerfectButton from "./components/PerfectButton";
+
+const App = () => {
+  return (
+    <main className="h-screen w-full justify-center flex items-center">
+      <PerfectButton />
+    </main>
+  );
+};
+
+export default App;
